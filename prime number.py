ef3c1d936bbdb222dcd492f86b45e222664241c4
pushpa424 <149574145+pushpa424@users.noreply.github.com>
2026-01-22 18:14:03 +0530

    Add files via upload

---
+++ prime number.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a num:"))
+count = 0
+for i in range(1,num+1):
+    if num % i == 0:
+        count = count + 1
+if count == 2:
+    print("num is prime")
+else:
+    print("num is not a prime")
